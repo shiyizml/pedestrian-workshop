8056c907c10ccde05bedcaccd012381807a74961
shiyizml <shiyizml@gmail.com>
2026-08-11 22:30:16 +0800

    Publish Pedestrian Workshop on GitHub Pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Body as Brush, Gesture as Landscape",
+  title: "Pedestrian Workshop",
   description:
     "A bilingual XR interactive art project translating Peking Opera gestures into Chinese landscape painting.",
 };
